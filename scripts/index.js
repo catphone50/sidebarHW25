0585814ab8be95c82ac5d7b79bd89040f82e49f3
catphone50 <catphone50@gmail.com>
2024-07-11 17:20:03 +0200

    add subMenu

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector(".menu");
 const closeButton = document.querySelector(".close");
 const navSide = document.querySelector(".navSide");
+const btns = document.querySelectorAll(".btn");
 
 menuButton.addEventListener("click", () => {
   navSide.classList.add("active");
@@ -8,4 +9,13 @@ menuButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   navSide.classList.remove("active");
+});
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const subList = btn.nextElementSibling;
+    if (subList) {
+      subList.classList.toggle("show");
+    }
+  });
 });
